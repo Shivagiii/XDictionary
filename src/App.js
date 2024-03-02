c932c64d0bf06f8dd90db0db77c8394fa4042856
Shivagiii <shivangiadeshara@gmail.com>
2024-03-02 11:34:05 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text,setWord] = useState('');
+  const [found,setFound]=useState([])
+  
+  const dictionary=
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ]
+  
+  function handleSearch(e){
+    e.preventDefault();
+    let meanings=dictionary.find((words) => words.word.toLowerCase() === text.toLowerCase())  
+   setFound(meanings)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Dictionary App</h2>
+      <form onSubmit={handleSearch}>
+      <input type="text" value={text} placeholder="Search for a word" onChange={(e) => setWord(e.target.value)}/>
+      <button type="submit" >Search</button>
+      </form>
+      <h4>Defination:</h4>
+      <p>{found? found.meaning:"Word not found in the dictionary."}</p>     
     </div>
   );
 }
